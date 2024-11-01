c0756c3c285ed19f0ad04cb4c057ef379c1a1122
Dnjoc <dnjoc.dc@gmail.com>
2024-11-01 19:37:24 -0400

    updated

--- index.js
+++ index.js
@@ -161,5 +161,5 @@ app.use(errorHandler)
 //const PORT = process.env.PORT || 3001
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-  //console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
